dedca7252042a7dfb1b524c7e50fca8154517311
AustinPaley <austinpaley32@gmail.com>
2018-04-05 12:30:02 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else return false
+    end
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    if array.length == 5
+      array.length - 1
+    else array.length
+    end
   end
 end
